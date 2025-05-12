8f39d36e0755230ff38e3aba83e464a33515271a
heianshazhudao <1515696247@qq.com>
2025-05-13 00:35:27 +0800

    基本的程序和入口点

---
+++ Sandbox/src/SandBoxApp.cpp
@@ -0,0 +1,11 @@
+#include<Hazel.h>
+
+class SandBox :public Hazel::Application {
+public:
+	SandBox() {}
+	~SandBox() {}
+};
+
+Hazel::Application* Hazel::CreateApplication() {
+	return new SandBox();
+}
